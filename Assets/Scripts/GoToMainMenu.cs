f7e3196153bbf9ea052337e392a308045298839c
sasposaspo <spopovici@my.waketech.edu>
2024-12-10 20:21:33 -0500

    UI Scaling

---
+++ Assets/Scripts/GoToMainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToMainMenu : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(LoadMainMenu());
+    }
+    
+    IEnumerator LoadMainMenu()
+    {
+        yield return new WaitForSeconds(25);
+        SceneManager.LoadScene("Cinematic 2");
+    
+    }
+}
